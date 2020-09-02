fe863c9b653d5d072d47deb2e4aab398e3f37404
Kimfucious <kimfucious@abts.io>
2020-09-02 19:08:08 +0700

    add new test suite for React Router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AnotherPage } from "../pages/AnotherPage";
 import { App } from "../pages/App";
-import { NotFound } from "../pages/NotFound";
-import { SignIn } from "../pages/SignIn";
-import { PublicRoute } from "./PublicRoute";
-import { PrivateRoute } from "./PrivateRoute";
 import { NavBar } from "../components/shared/NavBar";
-// import PrivateRoute from "./PrivateRoute";
-// import PublicRoute from "./PublicRoute";
-// import NavBar from "../components/shared/NavBar";
-// import Aloha from "../Aloha";
+import { NotFound } from "../pages/NotFound";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
+import { SignIn } from "../pages/SignIn";
 
 export const AppRouter = () => {
   return (
@@ -21,6 +18,10 @@ export const AppRouter = () => {
         <PrivateRoute path="/home">
           <NavBar />
           <App />
+        </PrivateRoute>
+        <PrivateRoute path="/another">
+          <NavBar />
+          <AnotherPage />
         </PrivateRoute>
         <Route>
           <NotFound />
