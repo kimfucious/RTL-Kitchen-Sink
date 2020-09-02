6a183ef77add6a5a3800b96007078a10f49a1a65
Kimfucious <kimfucious@abts.io>
2020-09-02 19:07:46 +0700

    clean up old boilerplate

--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -3,9 +3,11 @@ import React from "react";
 import sink from "../../images/sink_32.png";
 import { useDispatch } from "react-redux";
 import { signOut } from "../../actions.js";
+import { Link, useLocation } from "react-router-dom";
 
 export const NavBar = () => {
   const dispatch = useDispatch();
+  const location = useLocation();
   return (
     <nav className="navbar navbar-expand-lg navbar-dark animate__animated animate__fadeIn">
       <div className="container">
@@ -35,46 +37,24 @@ export const NavBar = () => {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            {/* <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li> */}
-            {/* <li className="nav-item">
-            <a className="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
+            <li
+              className={`nav-item ${
+                location.pathname === "/home" ? "active" : ""
+              }`}
             >
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">
-              Disabled
-            </a>
-          </li> */}
+              <Link className="nav-link" to="/home">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li
+              className={`nav-item ${
+                location.pathname === "/another" ? "active" : ""
+              }`}
+            >
+              <Link className="nav-link" to="/another">
+                Another Page
+              </Link>
+            </li>
           </ul>
           <button
             className="btn btn-outline-primary my-2 my-sm-0"
